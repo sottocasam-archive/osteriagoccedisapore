39fc404a52337ad48dda9192c4d21dd2f25133e7
Sottocasa Michele <michelesottocasa1@gmail.com>
2022-05-16 23:38:31 +0200

    Updating menu

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const menuBtn = document.querySelector('.menu-btn');
-            const menu = $('#menu')[0];
-            let menuOpen = false;
+const menu = $('#menu')[0];
+let menuOpen = false;
 
-            if(innerWidth < 768) menu.classList.add('animate__zoomOutLeft');
+if (innerWidth < 768) menu.classList.add('animate__zoomOutLeft');
 
-            menuBtn.addEventListener('click', () => {
-                if (!menuOpen) {
-                    menuBtn.classList.add('open');
-                    menu.classList.add('menu__open', 'animate__zoomInLeft');
-                    menu.classList.remove('animate__zoomOutLeft');
-                    menuOpen = true;
-                } else {
-                    menuBtn.classList.remove('open');
-                    menu.classList.add('animate__zoomOutLeft');
-                    setTimeout(() => {menu.classList.remove('menu__open','animate__zoomInLeft');}, 1000);
-                    menuOpen = false;
-                }
-            });
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menu.classList.add('menu__open', 'animate__zoomInLeft');
+        menu.classList.remove('animate__zoomOutLeft');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menu.classList.add('animate__zoomOutLeft');
+        setTimeout(() => { menu.classList.remove('menu__open', 'animate__zoomInLeft'); }, 1000);
+        menuOpen = false;
+    }
+});
 
-            function toggleMenu() {
-                //alert(2)
-                $("#menu").toggle;
-            }
+function toggleMenu() {
+    //alert(2)
+    $("#menu").toggle;
+}
